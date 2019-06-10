9ef05feb3d3c71d3505167cafdae5c86fe06fc61
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-06-10 16:37:59 -0500

    finished tree and added testing

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -1,17 +1,78 @@
 'use strict';
 
-//* NODE CLASS
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-//* TREE CLASS
-class Tree {
-    constructor() {
-        this.root = null;
+let inOrderArr = [];
+let preOrderArr = [];
+let postOrderArr = [];
+
+module.exports = class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+
+    if(this.root === null)
+      this.root = newNode;
+    else
+      this.insertNode(this.root, newNode);
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if(node.left === null)
+        node.left = newNode;
+      else
+        this.insertNode(node.left, newNode);
     }
-}
+    else
+    if(node.right === null)
+      node.right = newNode;
+    else
+      this.insertNode(node.right, newNode);
+  }
+
+  getRootNode() {
+    return this.root;
+  }
+
+  inorder(node) {
+    if(node !== null)
+    {
+      this.inorder(node.left);
+      inOrderArr.push(node.value);
+      this.inorder(node.right);
+      return inOrderArr;
+    }
+  }
+
+  preorder(node) {
+    if(node !== null)
+    {
+      preOrderArr.push(node.value);
+      this.preorder(node.left);
+      this.preorder(node.right);
+      return preOrderArr;
+    }
+
+
+  }
+
+  postorder(node) {
+    if(node !== null)
+    {
+      this.postorder(node.left);
+      this.postorder(node.right);
+      postOrderArr.push(node.value);
+      return postOrderArr;
+    }
+  }
+};
---
+++ challenges/tree/tree.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Tree = require('./tree');
+
+let seed = new Tree;
+
+describe('methods of a tree', () => {
+
+  it('Can create a new tree', () => {
+    seed.insert(1);
+    seed.insert(2);
+    seed.insert(3);
+    seed.insert(1234);
+    seed.insert(0);
+    expect(seed).toBeDefined();
+    expect(seed.root.value).toBe(1);
+    expect(seed.root.left.value).toBe(0);
+    expect(seed.root.left.left).toBe(null);
+    expect(seed.root.right.value).toBe(2);
+    expect(seed.root.right.right.value).toBe(3);
+    expect(seed.root.right.right.right.value).toBe(1234);
+  });
+
+  it('Can traverse in order', () => {
+    expect(seed.inorder(seed.root)).toEqual([0,1,2,3,1234]);
+  });
+
+  it('Can traverse pre order', () => {
+    expect(seed.preorder(seed.root)).toEqual([1,0,2,3,1234]);
+  });
+
+  it('Can traverse post order', () => {
+    expect(seed.postorder(seed.root)).toEqual([0,1234,3,2,1]);
+  });
+
+
+});
