8029c60c214a065b3a14df065dc363a69b24bd79
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-06-10 14:10:39 -0500

    Added basic tree/node class

---
+++ challenges/tree/tree.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//* NODE CLASS
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+//* TREE CLASS
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+}
